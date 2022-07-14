026a7fbba47e2550e7eeb4f9a848cfdcdb93d44e
Joeri Abbo <joeriabbo@hotmail.com>
2022-07-14 21:32:14 +0200

    ICTTrainingen : Breadth-first Traversal.

--- src/com/skillsoft/datastructures/Main.java
+++ src/com/skillsoft/datastructures/Main.java
@@ -1,6 +1,31 @@
 package com.skillsoft.datastructures;
 
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class Main {
+
+    public static <T> void breadthFirst(Node<T> root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node<T>> queue = new LinkedList<>();
+
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node<T> node = queue.remove();
+            System.out.print(node + "->");
+
+            if (node.getLeftChild() != null) {
+                queue.add(node.getLeftChild());
+            }
+
+            if (node.getRightChild() != null) {
+                queue.add(node.getRightChild());
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         Node<Character> a = new Node<>('A');
@@ -11,26 +36,36 @@ public class Main {
         Node<Character> f = new Node<>('F');
         Node<Character> g = new Node<>('G');
         Node<Character> h = new Node<>('H');
-
         a.setLeftChild(b);
         a.setRightChild(c);
+
+        System.out.println("\n");
+        breadthFirst(a);
 
         c.setLeftChild(d);
         c.setRightChild(e);
 
+        System.out.println("\n");
+        breadthFirst(a);
 
         d.setLeftChild(f);
         d.setRightChild(h);
 
+        System.out.println("\n");
+        breadthFirst(a);
+
         e.setRightChild(g);
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
-        System.out.println(f);
-        System.out.println(g);
-        System.out.println(h);
+        System.out.println("\n");
+        breadthFirst(a);
+        //
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(d);
+//        System.out.println(e);
+//        System.out.println(f);
+//        System.out.println(g);
+//        System.out.println(h);
     }
 }
