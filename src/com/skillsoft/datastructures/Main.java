685bfca63728f3e66658c7f57ec43cc072098630
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-16 21:31:34 +0200

    Icttrainingen : Operation on a maximum heap.

--- src/com/skillsoft/datastructures/Main.java
+++ src/com/skillsoft/datastructures/Main.java
@@ -579,18 +579,41 @@ public class Main {
         return -1;
     }
 
-    public static void main(String[] args) {
-        int[] sortedList = new int[]{10, 30, 40, 50, 60, 80, 90, 100, 110, 120, 130, 140, 160, 180};
+    public static void main(String[] args) throws HeapFullException, HeapEmptyException {
 
-        System.out.println(Arrays.toString(sortedList));
+        MaxHeap<Integer> maxHeap = new MaxHeap<>(Integer.class);
+//
+//        maxHeap.insert(9);
+//        System.out.println(maxHeap);
+//
+//        System.out.println();
+//        maxHeap.insert(4);
+//        System.out.println(maxHeap);
+//
+//        System.out.println();
+//        maxHeap.insert(17);
+//        System.out.println(maxHeap);
+//
+//        System.out.println();
+//        maxHeap.insert(6);
+//        System.out.println(maxHeap);
+//
+//        System.out.println();
+//        maxHeap.insert(100);
+//        System.out.println(maxHeap);
 
-        System.out.println();
+        maxHeap.insert(9);
+        maxHeap.insert(4);
+        maxHeap.insert(17);
+        maxHeap.insert(6);
+        maxHeap.insert(100);
+        maxHeap.insert(144);
+        maxHeap.insert(47);
+        maxHeap.insert(247);
+        System.out.println(maxHeap);
+        System.out.println("________________________________________________");
 
-
-        System.out.print("Element index: " + interpolationSearch(sortedList, 80));
-        System.out.print("Element index: " + interpolationSearch(sortedList, 90));
-        System.out.print("Element index: " + interpolationSearch(sortedList, 130));
-        System.out.print("Element index: " + interpolationSearch(sortedList, 122));
-
+        System.out.println(maxHeap);
+        System.out.println("Highest: " + maxHeap.removeHighestPriority());
     }
 }
