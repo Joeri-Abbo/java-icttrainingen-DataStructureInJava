a8bc2f817159ae070df6d53623ab61cd418b5989
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-17 22:56:17 +0200

    Icttrainingen : Implementing topological sort.

--- src/com/skillsoft/datastructures/Main.java
+++ src/com/skillsoft/datastructures/Main.java
@@ -1,9 +1,12 @@
 package com.skillsoft.datastructures;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
+import java.util.Queue;
 
 public class Main {
 
@@ -674,11 +677,64 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) throws HeapFullException, HeapEmptyException {
-        int[] array = {4, 6, 9, 2, 10, 56, 12, 5, 1, 17, 14};
-        System.out.println(Arrays.toString(array));
-        heapsort(array);
-        System.out.println(Arrays.toString(array));
+    public static List<Integer> sortGraph(Graph graph) {
+        LinkedList<Integer> queue = new LinkedList<>();
+        Map<Integer, Integer> indegreeMap = new HashMap<>();
 
+        for (int vertex = 0; vertex < graph.getNumVertices(); vertex++) {
+            int indegree = graph.getIndegree(vertex);
+            indegreeMap.put(vertex, indegree);
+
+            if (indegree == 0) {
+                queue.add(vertex);
+            }
+        }
+        List<Integer> sortedList = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+//            NOTE: If more than one elemeent exists then it means that the graph has more tahn one topological sort solution.
+            int vertex = queue.remove();
+
+            // NOTE: this is the equivalent of processing the list
+            sortedList.add(vertex);
+
+            List<Integer> adjacentVertices = graph.getAdjacentVertices(vertex);
+
+            for (int adjacentVertex : adjacentVertices) {
+                int updatedIndegree = indegreeMap.get(adjacentVertex) - 1;
+                indegreeMap.put(adjacentVertex, updatedIndegree);
+
+                if (updatedIndegree == 0) {
+                    queue.add(adjacentVertex);
+                }
+            }
+        }
+
+        if (sortedList.size() != graph.getNumVertices()) {
+            throw new RuntimeException("The graph had a cycle!");
+        }
+        return sortedList;
+    }
+
+    public static void main(String[] args) throws HeapFullException, HeapEmptyException {
+        Graph graph = new AdjacencyMatrixGraph(8, Graph.GraphType.DIRECTED);
+
+        graph.addEdge(2, 7);
+        graph.addEdge(0, 3);
+        graph.addEdge(0, 4);
+        graph.addEdge(0, 1);
+        graph.addEdge(2, 1);
+        graph.addEdge(1, 3);
+        graph.addEdge(3, 5);
+        graph.addEdge(3, 6);
+        graph.addEdge(4, 7);
+        graph.addEdge(7, 0);
+
+        graph.displayGraph();
+
+        System.out.println();
+        System.out.println("----------------------------------------------------");
+        System.out.println(sortGraph(graph));
+        System.out.println("----------------------------------------------------");
     }
 }
